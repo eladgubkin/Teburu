9eb972ee218a279f7e4f3a60e59726aa01855f4d
Elad Gubkin <Eladox3@gmail.com>
2022-05-05 15:12:49 +0300

    mocks

--- client/src/mocks/models/CustomerModel.ts
+++ client/src/mocks/models/CustomerModel.ts
@@ -1,14 +1,10 @@
 import faker from "@faker-js/faker";
+import { Customer } from "../../types/data.types";
 
-export const CustomerModel = {
-  id: () => faker.random.number(10000).toString(),
-  firstName: () => faker.name.firstName(),
-  lastName: () => faker.name.lastName(),
-  age: () => faker.datatype.number({ min: 18, max: 69 }),
-  email: () => faker.internet.email(),
-  phone: () => faker.phone.phoneNumber("+7 (###) ###-##-##"),
-  city: () => faker.address.cityName(),
-  company: () => faker.company.companyName(),
-  avatar: () => faker.image.avatar(),
-  information: () => faker.lorem.words(10),
-};
+// export const CustomerModel: Customer = {
+// id: () => "lol",
+// id: () => faker.random.numeric(6),
+// name: () => faker.name.findName(),
+// email: () => faker.internet.email(),
+// phoneNumber: () => faker.phone.phoneNumber("052-###-####"),
+// };
---
+++ client/src/types/data.types.ts
@@ -0,0 +1,34 @@
+export interface Customer {
+  id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+}
+
+export interface Request {
+  id: number;
+  title: string;
+  desc: string;
+  from: string;
+  img: string;
+  status: string;
+  createdAt: string;
+}
+
+export interface Status {
+  id: string;
+  status: string;
+}
+
+export interface Asset {
+  id: string;
+  assetName: string;
+  assetOwner: string;
+  address: string;
+}
+
+export interface Provider {
+  id: string;
+  providerName: string;
+  category: string;
+}
