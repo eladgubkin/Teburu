82a6174e41e670f6281b60348e476be555f6c84f
Elad Gubkin <Eladox3@gmail.com>
2022-05-09 21:40:54 +0300

    Mantine and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,26 @@
 import { Routes, Route } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import rtlPlugin from "stylis-plugin-rtl";
+
 import { Customers } from "./components/Customers";
 import { Requests } from "./components/Requests";
+import { Layout } from "./components/Layout";
 
 export const App = () => {
   return (
-    <>
+    <MantineProvider
+      emotionOptions={{ key: "mantine", stylisPlugins: [rtlPlugin] }}
+      theme={{
+        colorScheme: "dark",
+        dir: "rtl",
+      }}
+    >
       <Routes>
-        <Route path="/customers" element={<Customers />} />
-        <Route path="/requests" element={<Requests />} />
+        <Route element={<Layout />}>
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/requests" element={<Requests />} />
+        </Route>
       </Routes>
-    </>
+    </MantineProvider>
   );
 };
