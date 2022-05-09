0b0963739a3639b7dc1912f34ce47da7943a7da1
Elad Gubkin <Eladox3@gmail.com>
2022-05-09 21:40:59 +0300

    Create Layout.tsx

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core";
+
+import { Customers } from "./Customers";
+
+export function Layout() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  return (
+    <AppShell
+      styles={{
+        main: {
+          background: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      fixed
+      navbar={
+        <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+          <Text>Application navbar</Text>
+        </Navbar>
+      }
+      footer={
+        <Footer height={60} p="md">
+          Application footer
+        </Footer>
+      }
+      header={
+        <Header height={70} p="md">
+          <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+          </div>
+        </Header>
+      }
+    >
+      <Customers />
+    </AppShell>
+  );
+}
