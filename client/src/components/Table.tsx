51213c04bb0053f252ade5e476788e37bf169b08
Elad Gubkin <Eladox3@gmail.com>
2022-05-09 21:41:06 +0300

    Update Table.tsx

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useState, useEffect } from "react";
 import DataGrid, { SortColumn, SortIconProps } from "react-data-grid";
+import { Button } from "@mantine/core";
 
 const SortIcon = ({ sortDirection }: SortIconProps) => {
   return sortDirection !== undefined ? <>{sortDirection === "ASC" ? "\u2B9D" : "\u2B9F"} </> : null;
@@ -51,24 +52,18 @@ export const Table = ({ endpoint, columns }) => {
   }, [rows, sortColumns]);
 
   return (
-    <>
-      <DataGrid
-        className=""
-        columns={columns}
-        rows={sortedRows}
-        rowKeyGetter={rowKeyGetter}
-        onRowsChange={setRows}
-        sortColumns={sortColumns}
-        onSortColumnsChange={setSortColumns}
-        selectedRows={selectedRows}
-        onSelectedRowsChange={setSelectedRows}
-        components={{ sortIcon: SortIcon }}
-        direction={"rtl"}
-      />
-
-      <button onClick={() => setRows(rows.filter((row) => row.email.includes("gmail")))}>
-        Click
-      </button>
-    </>
+    <DataGrid
+      className=""
+      columns={columns}
+      rows={sortedRows}
+      rowKeyGetter={rowKeyGetter}
+      onRowsChange={setRows}
+      sortColumns={sortColumns}
+      onSortColumnsChange={setSortColumns}
+      selectedRows={selectedRows}
+      onSelectedRowsChange={setSelectedRows}
+      components={{ sortIcon: SortIcon }}
+      direction={"rtl"}
+    />
   );
 };
